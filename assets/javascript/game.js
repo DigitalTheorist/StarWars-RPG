ddbca56a1ce667a882d24dc29e880599b9422c02
jcostopulos <costopulos@gmail.com>
2017-05-14 22:40:56 -0700

    initial responsive design complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+//PSEUDOCODE
+
+//onclick - select character and move to "playerchoice" section.
+
+//onclick - select opponent and move to opponent section.
+//---------------------------------
+
+//Attack by clicking attack button.
+
+//attack damages defender a random number based on attack strenth?
+
+//eact character has three attributes.
+var healthPoints
+var attackPower
+// when character attacks attackPower increases by its base attackPower (attackPower += attackPower)
+var counterAttackPower
+// opponent character only has counterAttackPower - never changes. (compared to attachPower in attack click function)
+
+
+//player clicks damages defender. opponent loses health points. (displayed at bottom of characters picture)
+
+
+//opponent instantly counter attacks causing player character to lose HP shown at the bottom of the character's picture.
+
+// repeat the attack sequence
+
+// when opponents HP is reduced to zero or below remove that character from the opponent area - player picks a new opponent
+
+// player wins if all enemies are defeated - loses if HP falls to zero or below.
+
+// Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
